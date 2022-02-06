d90df5a9a1f9df08b36cdd2e2d1e67c1f175be6a
Timur Issaev <timka_issaev@mail.ru>
2022-02-06 15:35:16 +0300

    improved added spaces

--- application/controllers/controller_main.php
+++ application/controllers/controller_main.php
@@ -17,12 +17,12 @@ class Controller_Main extends Controller {
     $data = array();
     $param = array();
     $param['page_title'] = 'Добро пожаловать | '.SITE_NAME;
-    if(empty($_GET['category'])) {
-  $category=0;
-} else {
 
-  $category=$_GET['category'];
-}
+    if(empty($_GET['category'])) {
+      $category=0;
+    } else {
+      $category=$_GET['category'];
+    }
 
     $data['foods'] = $this->model->getFoods($category);
     $this->view->generate('main/main_view.php', '/templates/template_view.php', $param, $data, null);
@@ -32,12 +32,13 @@ class Controller_Main extends Controller {
     $data = array();
     $param = array();
     $param['page_title'] = 'Добро пожаловать | '.SITE_NAME;
-    if(empty($_GET['food_id'])) {
-  $food_id=0;
-} else {
 
-  $food_id=$_GET['food_id'];
-}
+    if(empty($_GET['food_id'])) {
+      $food_id=0;
+    } else {
+      $food_id=$_GET['food_id'];
+    }
+
     $data['foods'] = $this->model->getFoodInfo($food_id);
     $this->view->generate('main/food_view.php', '/templates/template_view.php', $param, $data, null);
   }
@@ -50,69 +51,29 @@ class Controller_Main extends Controller {
     $data = array();
     $param = array();
     $param['page_title'] = 'Добро пожаловать | '.SITE_NAME;
-    //$data['foods'] = $this->model->getFoodInfo($_GET['food_id']);
-
-
-/*
-  if(!isUserAuth()) {
-    header('Location: /login.php');
-  }
-  if(!empty($_POST['create_food_submit'])) {
-    
-  createFood($_POST['create_food_title'],$_POST['create_food_description']);
-  }
-*/
-if(!empty($_POST['create_food_submit'])) {
-
-$data['foods'] = $this->model->createFood($_POST['create_food_title'], $_POST['create_food_description'], $_POST['create_food_category'],$_POST['food_photo_path_0']);
-}
-
-
-
+    if(!empty($_POST['create_food_submit'])) {
+      $data['foods'] = $this->model->createFood($_POST['create_food_title'], $_POST['create_food_description'], $_POST['create_food_category'],$_POST['food_photo_path_0']);
+    }
     $this->view->generate('main/create_food_view.php', '/templates/template_view.php', $param, $data, null);
   }
 
-
-
   public function action_ajax_photo_upload() {
-$param = array();
-$data = array();
-$uploaddir = 'public/';
-$uploadfile = $uploaddir . md5(time().rand().$_SERVER['REMOTE_ADDR']).'.jpg';
-
-//echo '<pre>';
-if (copy($_FILES['userfile']['tmp_name'], $uploadfile)) {
-   // echo "Файл корректен и был успешно загружен.\n";
-    echo json_encode(array('path' => $uploadfile));
-} else {
-   // echo "Возможная атака с помощью файловой загрузки!\n";
-}
-
-//echo 'Некоторая отладочная информация:';
-//print_r($_FILES);
-
-//print "</pre>";
-
-
-
-   // $this->view->generate('main/create_food_view.php', '/templates/template_view.php', $param, $data, null);
+    $param = array();
+    $data = array();
+    $uploaddir = 'public/';
+    $uploadfile = $uploaddir . md5(time().rand().$_SERVER['REMOTE_ADDR']).'.jpg';
+    if (copy($_FILES['userfile']['tmp_name'], $uploadfile)) {
+      echo json_encode(array('path' => $uploadfile));
+    } else {
+      echo json_encode('is_error'=>true);
+    }
   }
-
-
-
   
   public function action_ajax_delete_my_post() {
     $data = array();
     $param = array();
-    /*if(!isset($_POST['target_id']) || !isset($_POST['text'])) {
-      $data['ajax'] =array('is_error'=>true);
-    } else {*/
-      $data['ajax'] = $this->model->deleteMyPost($_GET['post_id']);
-    //}
+    $data['ajax'] = $this->model->deleteMyPost($_GET['post_id']);
     $this->view->generate('', '/ajax/json_response.php', $param, $data, null);
   }
-
-
-
 
 }
